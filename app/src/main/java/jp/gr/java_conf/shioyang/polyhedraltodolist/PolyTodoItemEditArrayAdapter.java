a684d0b8edb2ad33d6f81a13bf8c005eaaae636d
shioyang <shioyang4321@gmail.com>
2015-09-01 08:49:26 +0900

    Assign updated title when "done" selected.

--- app/src/main/java/jp/gr/java_conf/shioyang/polyhedraltodolist/PolyTodoItemEditArrayAdapter.java
+++ app/src/main/java/jp/gr/java_conf/shioyang/polyhedraltodolist/PolyTodoItemEditArrayAdapter.java
@@ -2,11 +2,14 @@ package jp.gr.java_conf.shioyang.polyhedraltodolist;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -23,7 +26,7 @@ public class PolyTodoItemEditArrayAdapter extends ArrayAdapter<PolyTodoItem> {
         if (convertView == null)
             convertView = inflater.inflate(R.layout.list_item_edit, null);
 
-        PolyTodoItem polyTodoItem = getItem(position);
+        final PolyTodoItem polyTodoItem = getItem(position);
 
         // Color
         convertView.setBackgroundColor(polyTodoItem.getColor());
@@ -31,6 +34,17 @@ public class PolyTodoItemEditArrayAdapter extends ArrayAdapter<PolyTodoItem> {
         // Text
         EditText editText = (EditText)convertView.findViewById(R.id.editView);
         editText.setText(polyTodoItem.getJustTitle());
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                boolean done = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    polyTodoItem.setJustTitle(textView.getText().toString());
+                    done = true;
+                }
+                return done;
+            }
+        });
 
         // Add icon
 //        ImageButton addButton = (ImageButton)convertView.findViewById(R.id.addButton);
